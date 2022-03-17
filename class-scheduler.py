eceec78cd6708dc0252dc3414651b46c188b2a72
Tara <taratrody@gmail.com>
2022-03-16 23:59:07 -0400

    find elements using the By module

--- class-scheduler.py
+++ class-scheduler.py
@@ -8,18 +8,25 @@ browser = webdriver.Chrome(driver_path)
 browser.get(('https://t.mmears.com/v2/home'))
 
 # HTML elements
-sign_in_button = browser.find_element_by_class_name("sign--26Rs4")
-username_field = browser.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/div/div[2]/div[1]/input')
-password_field = browser.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/div/div[2]/div[2]/input')
-sign_in_submit = browser.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/div/div[3]/div/div/div')
-pop_up = browser.find_element_by_xpath('//*[@id="booknowWrapper"]/div[3]/div/div[2]/div/div/img')
-confirm_button = browser.find_element_by_xpath('//*[@id="booknowWrapper"]/div[2]/div[2]/div[2]/div[1]')
-
+sign_in_button = browser.find_element(By.CLASS_NAME, 'sign--26Rs4')
 sign_in_button.click()
+
+username_field = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Email Address']")
 username_field.send_keys(username)
+
+password_field = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Password']")
 password_field.send_keys(password)
+
+sign_in_submit = browser.find_element(By.CLASS_NAME,'button--3uR3-')
 sign_in_submit.click()
-pop_up.click()
+# pop_up = browser.find_element_by_xpath('//*[@id="booknowWrapper"]/div[3]/div/div[2]/div/div/img')
+# confirm_button = browser.find_element_by_xpath('//*[@id="booknowWrapper"]/div[2]/div[2]/div[2]/div[1]')
+
+
+username_field.send_keys(username)
+
+sign_in_submit.click()
+# pop_up.click()
 
 
 """
