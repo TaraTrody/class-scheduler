1a9199c066073f1a2f8a6fd2b5a5aba3734b7eb7
Tara <taratrody@gmail.com>
2022-04-05 01:59:36 -0400

    defined func to get the next class time to be schedu

--- class-scheduler.py
+++ class-scheduler.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 from selenium.webdriver.common.action_chains import ActionChains 
-import sched,time,datetime
-from datetime import datetime, timedelta
+import sched,time,datetime, bisect
+from datetime import datetime as dt, timedelta
 from variables import driver_path, username, password
 
 
@@ -29,29 +29,51 @@ try:
     ActionChains(browser).move_to_element_with_offset(pop_up, -20, 0).click().perform()
     browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div').click()
 # Check day
-    time.sleep(4)
-    current_day = datetime.now()
+    time.sleep(4)   
+   
+    def get_next_class():
+        class_times = ['06:30', '07:00', '07:30', '08:00', '08:30', '09:00']
+        def convert_dates(class_time):
+             return dt.strptime(class_time, '%H:%M').time()
+        result  = map(convert_dates, class_times)
+        current_time = dt.now().time()
+        next_class = bisect.bisect_left(list(result),  current_time)
+        return class_times[next_class] + " am"
+
+    current_day = dt.now()
     delta = timedelta(hours=24)
     schedule_day = (current_day + delta).strftime('%A')
+    next_class = get_next_class()
     
    
-    current_day = current_day.strftime('%w')
+    current_day = current_day.strftime('%w') 
 
     if current_day == '5' or current_day == '6' or current_day == '0': 
         browser.find_element(By.XPATH, '//*[local-name()="svg" and @data-icon="right"]').click()
         schedule_day = 'Monday'
-    
-    xpath_str = "//p[normalize-space()='{0}']/parent::node()/following-sibling::div//p[@class='times-- 2iBSf']".format(schedule_day)
-    class_times = browser.find_elements(By.XPATH, xpath_str)  
+
+    xpath_str = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']".format(schedule_day, next_class)
+    class_times = browser.find_element(By.XPATH, xpath_str)  
     print(class_times)
 
-    for i in class_times:
-        WebDriverWait(browser,10).until(EC.element_to_be_clickable(i)).click()
-        browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
-        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[normalize-space()="No, thanks!"]'))).click()
+    # for i in class_times:
+    #     WebDriverWait(browser,10).until(EC.element_to_be_clickable(i)).click()
+    #     browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
+    #     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[normalize-space()="No, thanks!"]'))).click()
+    #     time.sleep(4)
+
+        
+# create loop with schedule function
     
-    
-    
+    # get a list of all the time elements under the title
+    # for each time 
+        # if the text isn't booked or standby booked 
+            #refresh page and click element
+            #click footer
+            #click dialog
+            # pause until some time (maybe reset current time and add 30
+            # )
+    # times_list = ['06:30', '07:00', '07:30', '08:00', '08:30']
     
 except Exception as exc:
     print(exc)
