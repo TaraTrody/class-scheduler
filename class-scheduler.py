a0bcfd52efab02d9e4f5d18922969bdb91053b15
Tara <taratrody@gmail.com>
2022-04-07 00:44:10 -0400

    clean up and add comments for FIXES & TODOs

--- class-scheduler.py
+++ class-scheduler.py
@@ -7,73 +7,74 @@ from selenium.webdriver.common.action_chains import ActionChains
 from apscheduler.schedulers.blocking import BlockingScheduler
 import time,datetime, bisect
 from datetime import datetime as dt, timedelta
-from variables import driver_path, username, password
+from variables import username, password # NOTE: Can this come from the system environ variables?
 
 
 browser = webdriver.Chrome()
 browser.get(('https://t.mmears.com/v2/home'))
-
-# Log in to account
-browser.find_element(By.CLASS_NAME, 'sign--26Rs4').click()
-username_field = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Email Address']").send_keys(username)
-password_field = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Password']").send_keys(password)
-sign_in_submit = browser.find_element(By.CLASS_NAME,'button--3uR3-').click()
-
 sched = BlockingScheduler()
 
-
-# Remove Modal
 try: 
-    time.sleep(8) # is there more efficient way do I need the EC should I use implicit wait?
+    # Log in to account
+    browser.find_element(By.CLASS_NAME, 'sign--26Rs4').click()
+    username_field = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Email Address']").send_keys(username)
+    password_field = browser.find_element(By.CSS_SELECTOR, "input[placeholder='Password']").send_keys(password)
+    sign_in_submit = browser.find_element(By.CLASS_NAME,'button--3uR3-').click()
+
+    # Removes the modal  
+    time.sleep(8) # NOTE: is there better way to implement this?
+    # FIXME: refactor to combine element present_present & popup
     element_present = EC.visibility_of_element_located((By.XPATH, '//body/div/div/div/div[3]/div/div[2]')) 
     WebDriverWait(browser, 10).until(element_present)
     pop_up = browser.find_element(By.XPATH, '//body/div/div/div/div[3]/div/div[2]')
     ActionChains(browser).move_to_element_with_offset(pop_up, -20, 0).click().perform()
     browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div').click()
 
-# Define job. This is a function that will select and schedule a class
- 
+   
     def schedule_class(): 
+        # This function defines the job the will select and schedule the upcoming class
         browser.refresh()
-        
+            
         def get_next_class():
-            # class_times = ['06:30', '07:00', '07:30', '08:00', '08:30', '09:00']
-            # def convert_dates(class_time):
-            #      return dt.strptime(class_time, '%H:%M').time()
-            # result  = map(convert_dates, class_times)
-            # current_time = dt.now().time()
-            # next_class = bisect.bisect_left(list(result),  current_time)
-            # return class_times[next_class] + " am"
-            return "06:30 am"
+            # returns the next class time to be scheduled
+            class_times = ['06:30', '07:00', '07:30', '08:00', '08:30', '09:00']
+            def convert_dates(class_time):
+                return dt.strptime(class_time, '%H:%M').time()
+            result  = map(convert_dates, class_times)
+            current_time = dt.now().time()
+            next_class = bisect.bisect_left(list(result),  current_time)
+            return class_times[next_class] + " am"
+        
 
         today = dt.now()
-        delta = timedelta(hours=48)
+        delta = timedelta(hours=24) # classes scheduled 24hrs in advance
         schedule_day = (today + delta).strftime('%A')
         next_class = get_next_class()
         
 
         current_day = today.strftime('%w') 
 
-    # Click to next page to schedule the Monday class 
+        # Click to next page to schedule the Monday class 
         if current_day == '5' or current_day == '6' or current_day == '0': 
             browser.find_element(By.XPATH, '//*[local-name()="svg" and @data-icon="right"]').click()
             schedule_day = 'Monday'
-
+        # Get the class element
         xpath_str = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']".format(schedule_day, next_class)
-        class_times = browser.find_element(By.XPATH, xpath_str) 
-        successful_schedule = class_times.text 
+        class_times = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, xpath_str)))
         
+        # TODO update with logic to check if the sibling of the class time is Not "Booked" continue to schedule class
 
+        # Clicks the class time, confirmation button and closes the pop up
         WebDriverWait(browser, 10). until(EC.element_to_be_clickable(class_times)).click()
         browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
         WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[normalize-space()="No, thanks!"]'))).click()
-
-        print(f"Great! you you're schedule for the {successful_schedule} class")
+        
+        print("Great! you you're schedule for the class")
 
     current_date = dt.now().date() 
-    start = f"{current_date} 23:37:00"
-    end = f"{current_date} 23:39:00"    
-
+    start = f"{current_date} 00:19:00"
+    end = f"{current_date} 00:22:00"    
+    # FIXME: scheduler not ending after endtime
     sched.add_job(schedule_class, 'interval', minutes=1, start_date=start, end_date=end)    
     sched.start()
 
