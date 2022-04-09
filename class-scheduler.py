3ce5f6d9e6bd9c23a68df8d9860915669a4c9231
Tara <taratrody@gmail.com>
2022-04-08 23:56:47 -0400

    Add logic to check if class is not already booked before booking

--- class-scheduler.py
+++ class-scheduler.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from apscheduler.schedulers.blocking import BlockingScheduler
 import time,datetime, bisect
 from datetime import datetime as dt, timedelta
-from variables import username, password # NOTE: Can this come from the system environ variables?
+from variables import username, password # NOTE 1: Can this come from the system environ variables?
 
 
 browser = webdriver.Chrome()
@@ -64,22 +64,24 @@ try:
         xpath_str = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']".format(schedule_day, next_class)
         class_times = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, xpath_str)))
         
+        # TODO 1: update with logic to check if the sibling of the class time is Not "Booked" continue to schedule class
         xpath_status = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']/following-sibling::p".format(schedule_day, next_class)
         status = WebDriverWait(browser, 10).until(EC.presence_of_element_located(By.XPATH, xpath_status ))
-
-        # Clicks the class time, confirmation button and closes the pop up
-        WebDriverWait(browser, 10). until(EC.element_to_be_clickable(class_times)).click()
-        browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
-        # TODO: Get the correct element that pops up after scheduling a standby class
-        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[normalize-space()="No, thanks!"]'))).click()
         
-        print("Great! you're schedule for the class")
+        if  status.text != 'Booked':
+            # Clicks the class time, confirmation button and closes the pop up
+            WebDriverWait(browser, 10). until(EC.element_to_be_clickable(class_times)).click()
+            browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
+            # TODO 2: Get the correct element that pops up after scheduling a standby class
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[normalize-space()="No, thanks!"]'))).click()
+            
+            print("Great! you're schedule for the class")
 
-    # TODO: update with correct interval, start and end times
+    # TODO 3: update with correct interval, start and end times
     current_date = dt.now().date() 
     start = f"{current_date} 00:19:00"
     end = f"{current_date} 00:22:00"    
-    # FIXME: scheduler not ending after endtime
+    # FIXME 1: scheduler not ending after endtime
     sched.add_job(schedule_class, 'interval', minutes=1, start_date=start, end_date=end)    
     sched.start()
 
