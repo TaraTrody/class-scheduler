bf585be77602b04140b183bb7eadcccd203c73ee
Tara <taratrody@gmail.com>
2022-04-14 17:05:39 -0400

    refactored script and added a check for "main " variable

--- class-scheduler.py
+++ class-scheduler.py
@@ -4,87 +4,82 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains 
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-import time,datetime, bisect
+from apscheduler.schedulers.background import BackgroundScheduler
+import time, bisect
 from datetime import datetime as dt, timedelta
 from variables import username, password # NOTE 1: Can this come from the system environ variables?
 
-
-browser = webdriver.Chrome()
-browser.get(('https://t.mmears.com/v2/home'))
-sched = BlockingScheduler()
-
-try: 
-    # Log in to account
+def signIn():
     browser.find_element(By.CLASS_NAME, 'sign--26Rs4').click()
     browser.find_element(By.CSS_SELECTOR, "input[placeholder='Email Address']").send_keys(username)
     browser.find_element(By.CSS_SELECTOR, "input[placeholder='Password']").send_keys(password)
     browser.find_element(By.CLASS_NAME,'button--3uR3-').click()
 
-    
-    time.sleep(5) 
-    # Removes the modal
+def removeModal():
     pop_up =  WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH,'//body/div/div/div/div[3]/div/div[2]')))
     ActionChains(browser).move_to_element_with_offset(pop_up, -20, 0).click().perform()
     browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div').click()
 
+def get_next_class():
+    class_times = ['06:30', '07:00', '07:30', '08:00', '08:30', '09:00']
+
+    def convert_dates(class_time):
+        return dt.strptime(class_time, '%H:%M').time()
+
+    result  = map(convert_dates, class_times)
+    current_time = dt.now().time()
+    next_class = bisect.bisect_left(list(result),  current_time)
+
+    return class_times[next_class] + " am"
+
+def schedule_class(next_class_time):
+    today = dt.now()
+    delta = timedelta(hours=24) # classes scheduled 24hrs in advance
+    schedule_day = (today + delta).strftime('%A')
+    next_class = next_class_time
+    
+    current_day = today.strftime('%w') 
+
+    # Clicks to next page to schedule the Monday class 
+    if current_day == '5' or current_day == '6' or current_day == '0': 
+        browser.find_element(By.XPATH, '//*[local-name()="svg" and @data-icon="right"]').click()
+        schedule_day = 'Monday'
+    browser.refresh()   
+    xpath_str = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']".format(schedule_day, next_class)
+    class_times = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, xpath_str)))
+
+    WebDriverWait(browser, 10). until(EC.element_to_be_clickable(class_times)).click()
+    browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
+    # FIXME: driver fails to find this element
+    WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, ' //div[contains(text(),"OK")]'))).click()
+    
+    print("Great! you're schedule for the class")
+
+if __name__ == "__main__":
+    sched = BackgroundScheduler()
+    browser = webdriver.Chrome()
+    browser.get(('https://t.mmears.com/v2/home'))
    
-    def schedule_class(): 
-       
-        browser.refresh()
-            
-        def get_next_class():
-            # returns the next class time to be scheduled
-            class_times = ['06:30', '07:00', '07:30', '08:00', '08:30', '09:00']
+    signIn()
+    time.sleep(5) 
+    removeModal()
+    next_class=get_next_class()
+    
 
-            def convert_dates(class_time):
-                return dt.strptime(class_time, '%H:%M').time()
-
-            result  = map(convert_dates, class_times)
-            current_time = dt.now().time()
-            next_class = bisect.bisect_left(list(result),  current_time)
-
-            return class_times[next_class] + " am"
-        
-
-        today = dt.now()
-        delta = timedelta(hours=24) # classes scheduled 24hrs in advance
-        schedule_day = (today + delta).strftime('%A')
-        next_class = get_next_class()
-        
-
-        current_day = today.strftime('%w') 
-
-        # Clicks to next page to schedule the Monday class 
-        if current_day == '5' or current_day == '6' or current_day == '0': 
-            browser.find_element(By.XPATH, '//*[local-name()="svg" and @data-icon="right"]').click()
-            schedule_day = 'Monday'
-            
-        
-        xpath_str = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']".format(schedule_day, next_class)
-        class_times = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, xpath_str)))
-        
-       
-        xpath_status = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']/following-sibling::p".format(schedule_day, next_class)
-        status = WebDriverWait(browser, 10).until(EC.presence_of_element_located(By.XPATH, xpath_status ))
-        
-        if  status.text != 'Booked':
-            # Clicks the class time, confirmation button and closes the pop up
-            WebDriverWait(browser, 10). until(EC.element_to_be_clickable(class_times)).click()
-            browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
-            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, ' //div[contains(text(),"OK")]'))).click()
-            
-            print("Great! you're schedule for the class")
-
-    # TODO 3: update with correct interval, start and end times
-    current_date = dt.now().date() 
-    start = f"{current_date} 00:19:00"
-    end = f"{current_date} 00:22:00"    
-    # FIXME 1: scheduler not ending after endtime
-    sched.add_job(schedule_class, 'interval', minutes=1, start_date=start, end_date=end)    
+    current_date = dt.now().date().strftime('%Y-%m-%d')
+    date_str = f"{current_date} {next_class[:5]}:00"
+    sched.add_job(schedule_class, 'date', run_date=date_str, args=[next_class])    
     sched.start()
 
-except Exception as exc:
-    print(exc)
+try:
+        """ Scheduler continues to run, even when the tasks is complete. This thread 
+        allows the program to shutdown 
+        """
+        for i in range(1,150):
+            time.sleep(2)
+        sched.shutdown()
+except (KeyboardInterrupt, SystemExit):
+        
+       sched.shutdown()
+    
 
-browser.quit()
