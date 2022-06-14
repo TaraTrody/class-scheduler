9fed1626b9451d26f0d104626cb3b0a78a42c41d
Tara <taratrody@gmail.com>
2022-06-14 17:53:14 -0400

    Add code to locate 'Standby Booked Text' after confirm

--- class-scheduler.py
+++ class-scheduler.py
@@ -70,22 +70,32 @@ def schedule_class(next_class_time):
 
         if current_day !="0":     
             browser.refresh()
+        else:
+            time.sleep(2)
 
+        
         xpath_str = "//p[normalize-space()='{}']/parent::node()/following-sibling::div//p[text()='{}']".format(
             schedule_day, next_class
         )
         class_times = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, xpath_str))
-        )
+        ) # The next line duplicates this
 
         WebDriverWait(browser, 10).until(EC.element_to_be_clickable(class_times)).click()
         browser.find_element(By.XPATH, '//div[normalize-space()="Confirm"]').click()
         time.sleep(6)
-        # FIXME: driver fails to find this element
+       
     
         WebDriverWait(browser, 20).until(
             EC.element_to_be_clickable((By.XPATH, ' //div[contains(text(),"OK")]'))
         ).click()
+
+        time.sleep(3)
+
+        confirm_xpath_str = "//p[contains(text(),'{}')]/parent::node()/following-sibling::div//p[text()='{}']//following-sibling::p/text()".format(schedule_day, next_class) 
+        confirm_class = WebDriverWait(browser,10).until(EC.presence_of_element_located((By.XPATH, confirm_xpath_str)))
+        print(confirm_class)
+
     except Exception as exc:
         raise exc
 
@@ -102,15 +112,24 @@ if __name__ == "__main__":
     signIn()
     time.sleep(5)
     removeModal()
-    next_class = get_next_class()
-    # next_class = "06:00 am"
+    # next_class = get_next_class()
+
+    page = browser.page_source
+    soup = BeautifulSoup(page, 'html.parser')
+
+    file = open('./page_source.html', 'w')
+    file.write(page)
+    file.close()
+
+    next_class = "06:00 am"
 
     current_date = dt.now().date().strftime("%Y-%m-%d") 
-    date_str = f"{current_date} {next_class[:5]}:00"
-    # date_str = "2022-06-13 23:02:00"
+    # date_str = f"{current_date} {next_class[:5]}:00"
+    date_str = "2022-06-14 10:09:30"
     
     sched.add_listener(listener, EVENT_JOB_ERROR)
     sched.add_job(schedule_class, "date", run_date=date_str, args=[next_class])
+
 
     sched.start()
 
@@ -120,6 +139,6 @@ try:
     for i in range(1, 90):
         time.sleep(2)
     sched.shutdown()
-    browser.quit()
+    # browser.quit()
 except (KeyboardInterrupt, SystemExit):
     sched.shutdown()
